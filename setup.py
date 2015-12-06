d7661dcbd2a67293ac014c401fbb2a4acbe1ab2c
enricobacis <enrico.bacis@gmail.com>
2015-12-06 18:35:46 +0100

    Version 0.1: implement encode and decode

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='utf9',
+      version='0.1',
+      description='Encode and decode text with UTF-9 (IEEE RFC4042)',
+      install_requires=['bitarray'],
+      url='http://github.com/enricobacis/utf9',
+      author='Enrico Bacis',
+      author_email='enrico.bacis@gmail.com',
+      license='MIT',
+      packages=['utf9'],
+      keywords='utf9 encode decode rfc4042')
---
+++ utf9/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bitarray import bitarray as _bitarray
+
+def utf9encode(string):
+    bits = _bitarray()
+    for char in string:
+        for idx, byte in enumerate(char.encode('utf-8')):
+            bits.append(idx)
+            bits.extend('{0:b}'.format(ord(byte)).zfill(8))
+    return bits.tobytes()
+
+def utf9decode(data):
+    bits = _bitarray()
+    bits.frombytes(data)
+    chunks = (bits[x:x+9] for x in xrange(0, len(bits), 9))
+    string = u''
+    codepoint = ''
+    for chunk in chunks:
+        if len(chunk) < 9:
+            break
+        if chunk[0] == 0:
+            codepoint, string = '', string + codepoint.decode('utf-8')
+        codepoint += chr(int(chunk[1:].to01(), 2))
+    return string + codepoint.decode('utf-8')
+
+#print utf9decode(utf9encode(u'c豈iao'))
